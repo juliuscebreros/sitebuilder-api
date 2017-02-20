343c4736259a2974020f9103156ac05ce4988a5c
Julius <juliuscebreros@gmail.com>
2017-02-20 03:38:58 +0000

    Use promises instead

--- test/auth.spec.js
+++ test/auth.spec.js
@@ -11,10 +11,15 @@ describe.only( 'Auth', () => {
 
     it( 'should fail on no accessToken', ( done ) => {
         const evt = {}
-        auth.run( evt, ( err ) => {
-            should.exist( err );
-            return done();
-        });
+        auth.run( evt )
+            .then( ( test ) => {
+                console.log( 'Happened: ', test );
+            })
+            .catch(( err ) => {
+                console.log( 'Error? ', err );
+                should.exist( err );
+                return done();
+            });
     });
 
     it( 'should fail on expired access token', ( done ) => {
@@ -26,26 +31,38 @@ describe.only( 'Auth', () => {
 
         clock.tick( process.env.JWT_EXPIRY * 1000 );
 
-        auth.run( evt, ( err ) => {
-            clock.restore();
-            should.exist( err );
-            return done();
-        });
+        auth.run( evt )
+            .catch( ( err ) => {
+                clock.restore();
+                should.exist( err );
+                return done();
+            });
     });
 
     it( 'should succeed on correct jwt token', ( done ) => {
         const evt = {
             accessToken: auth.createToken( { id: 'one', username: 'juliusone' } )
         };
-        auth.run( evt, ( err, evt ) => {
-            if ( err ) {
+        auth.run( evt )
+            .then(( evt ) => {
+                should.exist( evt.req );
+                evt.req.user.id.should.equal( 'one' );
+                evt.req.user.username.should.equal( 'juliusone' );
+                return done();
+            } )
+            .catch( ( err ) => {
                 return done( err );
-            }
+            });
+    });
 
-            should.exist( evt.req );
-            evt.req.user.id.should.equal( 'one' );
-            evt.req.user.username.should.equal( 'juliusone' );
-            return done();
-        } );
+    it( 'should fail on invalid user', ( done ) => {
+        const evt = {
+            accessToken: auth.createToken( { id: 'nooneishere', username: 'whatthehell' } )
+        };
+        auth.run( evt )
+            .catch( ( err ) => {
+                should.exist( err );
+                return done();
+            });
     });
 });
